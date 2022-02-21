b79f8c98da5390420519da327411b46293348c24
taka74 <takahashi.nx7.1013@gmail.com>
2022-02-22 00:19:00 +0900

    時計実装

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
+from time import strftime
 from kivy.app import App
+from kivy.clock import Clock
 from kivy.lang import Builder
 from kivy.uix.label import Label
 
 class MainApp(App):
-    pass
+    def update_time(self, nap):
+        self.root.ids.time.text = strftime("%H:%M:%S") 
 
+    def on_start(self):
+        Clock.schedule_interval(self.update_time, 1)
+        
 if __name__ == '__main__':
-    
     MainApp().run()
